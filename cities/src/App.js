e4664f3f1880b1edfaade23fe0424b1895858270
Oleg <oleg.nod@gmail.com>
2019-01-12 15:40:17 +0300

    react weather undone

---
+++ cities/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+      <div>hello</div>
+      <h2>hello</h2>
+      <p>hello</p>
+      </div>
+      );
+  }
+}
+export default App;
