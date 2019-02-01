2cd3e010fa8bd5a596fbe2c2791dc38c240c503b
Oleg <oleg.nod@gmail.com>
2019-02-01 14:50:32 +0300

    added api,button etc

--- cities/src/App.js
+++ cities/src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Info from './components/info';
 import Form from './components/form';
 import Weather from './components/weather';
+
+const API_KEY='e0c9e1d19574294f950ce907b3114cef';
+
 class App extends React.Component {
+
+gettingWeather= async(e) => {
+  e.preventDefault();
+  const api_url= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+  const data= await api_url.json();
+  console.log(data);
+}
+
   render(){
     return(
       <div>
         <Info />
-        <Form />
+        <Form weatherMethod={this.gettingWeather} />
         <Weather />
       </div>
       );
